c5fd9cb8e361713e08115b2d91760fbb671b749e
MasterDoo <masterdu9951@icloud.com>
2018-05-31 00:56:17 -0400

    Adding Existing file

---
+++ Time_series/collect_time_table.m
@@ -0,0 +1,26 @@
+% Collect time timetable values from csv.
+
+% Name of variable to be loaded into workspace.
+names = {
+	'UNRATE',
+	'M2NS',
+	'M2',
+	'CPIAUCSL'
+	};
+
+collect = {};
+
+% Load data to workspace.
+for i = 1: length(names)
+	table = readtable([names{i}, '.csv']);
+	tt = table2timetable(table);
+	collect{i} = tt;
+end
+
+data_tt = synchronize(collect{:});
+data_tt = rmmissing(data_tt); % Remove missing variables.
+
+data_tb = timetable2table(data_tt);
+data_ar = table2array(data_tb(:, 2:end)); % Collection of numerical data
+% with DATE variable removed.
+
--- Time_series/dexuseu_net.m
+++ Time_series/dexuseu_net.m
@@ -3,27 +3,27 @@ clc;
 
 %% Loading data
 addpath("data");
-modelPara.dataName = "UNRATE";
+modelPara.dataName = "DEXUSEU";
 fetch_data;
 
 %% Initial visualization
 subplot(3, 1, 1);
-plot(data.main, "-", "Linewidth", 1.2);
+plot(data.main);
 xlabel("Time");
-ylabel("UNRATE");
-title("Civilian Unemployment Rate (UNRATE)");
+ylabel("DEXUSEU");
+title(" U.S. / Euro Foreign Exchange Rate (DEXUSEU)");
 
 subplot(3, 1, 2);
-plot(data.d1, "-", "Linewidth", 1.2);
+plot(data.d1, "c-");
 xlabel("Time");
-ylabel("d1 UNRATE");
-title("First Order Differencing Civilian Unemployment Rate (UNRATE)");
+ylabel("d1 DEXUSEU");
+title(" U.S. / Euro Foreign Exchange Rate (DEXUSEU)");
 
 subplot(3, 1, 3);
-plot(data.d2, "-", "Linewidth", 1.2);
+plot(data.d2, "g-");
 xlabel("Time");
-ylabel("d2 UNRATE");
-title("Second Order Differncing Civilian Unemployment Rate (UNRATE)");
+ylabel("d2 DEXUSEU");
+title(" U.S. / Euro Foreign Exchange Rate (DEXUSEU)");
 
 %% Creating data sets
 numTimeStepsTrain = floor(0.7 * data.len);
@@ -34,7 +34,7 @@ YTrain = data.main(:, 2: numTimeStepsTrain + 1);
 XTest = data.stack(:, numTimeStepsTrain + 1: end - 1);
 YTest = data.main(:, numTimeStepsTrain + 2: end);
 
-%% Standardize data (Mean Normalization)
+%% Standardize data
 mu = mean(XTrain')';
 sig = std(XTrain')';
 
@@ -49,27 +49,25 @@ numResponses = 1;
 
 numHiddenUnits.lstm1 = 64;
 numHiddenUnits.lstm2 = 32;
-numHiddenUnits.fc1 = 64;
-numHiddenUnits.fc2 = 32;
+numHiddenUnits.fc1 = 32;
 
 layers = [...
 	sequenceInputLayer(inputSize, "Name", "Input layer")
 	lstmLayer(numHiddenUnits.lstm1, "Name", "LSTM layer 1")
 	lstmLayer(numHiddenUnits.lstm2, "Name", "LSTM layer 2")
-	fullyConnectedLayer(numHiddenUnits.fc1, "Name", "FC layer 1")
+	fullyConnectedLayer(numHiddenUnits.fc1, "Name", "Full Connected layer 1")
 	fullyConnectedLayer(numResponses, "Name", "Output layer")
 	regressionLayer
 	];
 
 opts = trainingOptions(...
 	"adam",...
-	"MaxEpochs", 1500, ...
-	"GradientThreshold", 10, ...
+	"MaxEpochs", 250, ...
+	"GradientThreshold", 1, ...
 	"InitialLearnRate", 0.005, ...
 	"LearnRateSchedule", "piecewise", ...
-	"LearnRateDropPeriod", 250, ...
+	"LearnRateDropPeriod", 50, ...
 	"LearnRateDropFactor", 0.2, ...
-    "MiniBatchSize", 48, ...
 	"Verbose", 0, ...
 	"Plot", "training-progress");
 
@@ -82,22 +80,14 @@ net = trainNetwork(...
 
 
 %% Predicting
-% net = predictAndUpdateState(net, XTrain);
-% 
-% YPred = [];
+net = predictAndUpdateState(net, XTrain);
+
+YPred = [];
 
 numTimeStepsTest = length(XTest);
 
-% for i = 1:numTimeStepsTest
-% 	[net, YPred(1, i)] = predictAndUpdateState(net, XTest(:, i));
-% end
-
-net = predictAndUpdateState(net, XTrain);
-
-[net, YPred] = predictAndUpdateState(net, cache);
-
-for i = 2:numTimeStepsTest
-	[net, YPred(1, i)] = predictAndUpdateState(net, YPred(1, i-1));
+for i = 1:numTimeStepsTest
+	[net, YPred(1, i)] = predictAndUpdateState(net, XTest(:, i));
 end
 
 % Unstandardize result
@@ -116,12 +106,12 @@ plot(YPred, "-", "LineWidth", 1.25)
 grid on
 hold off
 legend(["Observed" "Predicted"])
-ylabel("UNRATE(%)")
-title("Civilian Unemployment Rate")
+ylabel("DEXUSEU")
+title(" U.S. / Euro Foreign Exchange Rate (DEXUSEU)")
 
 subplot(2, 1, 2)
 stem(YPred - YTest)
-xlabel("Month")
+xlabel("Date")
 ylabel("Error")
 title("RMSE = " + rmse)
 
