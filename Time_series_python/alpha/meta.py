5ad0aab958c10c8f394dc190f73a4c0059ba7fd1
MasterDoo <masterdu9951@icloud.com>
2018-06-23 23:48:00 -0400

    Uploading from Mac

--- Time_series_python/alpha/meta.py
+++ Time_series_python/alpha/meta.py
@@ -5,14 +5,7 @@ from datetime import datetime
 from sklearn import preprocessing
 
 
-class SeriesNotFoundError(Exception):
-	"""
-	The Error Risen to indicate
-	"""
-	pass
-
-
-class parameter_control():
+class ParameterControl():
 	"""
 	This logger is used to collect all user-specified
 	parameters in training.
@@ -52,26 +45,79 @@ class parameter_control():
 			input("Training epochs: ")
 			)
 
+	def load_customized_parameters(self):
+		self.nn.inputs = 1
+		self.nn.hidden = [64, 128]
+		self.nn.output = 1
+		self.nn.reg_para 
+
+class SeriesNotFoundError(Exception):
+	"""
+	The Error Risen to indicate
+	"""
+	pass
 
 
-fred_url_package = {
-	"SP500": "https://fred.stlouisfed.org/series/SP500/downloaddata/SP500.csv",
-	"CPIAUCSL": "https://fred.stlouisfed.org/series/CPIAUCSL/downloaddata/CPIAUCSL.csv"
-}
+def add_regularization(loss: tf.Tensor) -> (tf.Tensor):
+	l2 = float(0.005) * sum(
+	    tf.nn.l2_loss(tf_var)
+	        for tf_var in tf.trainable_variables()
+	        if not ("noreg" in tf_var.name or "Bias" in tf_var.name)
+	)
+	return loss + l2
+
+def gen_loss_tensor(
+	y_hat: tf.Tensor,
+	y: tf.Tensor,
+	metric: str="mse",
+	) -> (tf.Tensor, str):
+	loss_pack = {
+		"sse": tf.reduce_sum(tf.square(y_hat - y), name="loss_sse"),
+		"mse": tf.reduce_mean(tf.square(y_hat - y), name="loss_mse"),
+		"rmse": tf.sqrt(tf.reduce_mean(tf.square(y_hat - y)), name="loss_rmse")
+	}
+	return loss_pack[metric], metric
 
 
-def fetch_fred_single(target: str):
-	print("Fetching data from Fred database...")
-	try:
-		url = fred_url_package[target]
-	except KeyError:
-		raise SeriesNotFoundError("Time series requested cannot be found in data base.")
+def load_data(
+	target: str,
+	source: str
+	) -> (pd.Series, np.ndarray):
+	"""
+		source:
+			"fred": download data from fred database.
+			"local": load data from local csv file.
+	"""
+	fred_url_package = {
+		"SP500": "https://fred.stlouisfed.org/series/SP500/downloaddata/SP500.csv",
+		"CPIAUCSL": "https://fred.stlouisfed.org/series/CPIAUCSL/downloaddata/CPIAUCSL.csv"
+	}
+	if source == "fred":
+		print("Fetching data from Fred database...")
+		try:
+			url = fred_url_package[target]
+			data = pd.read_csv(url, delimiter=",", index_col=0)
+		except KeyError:
+			raise SeriesNotFoundError(
+				"Time series requested not found in data base.")
+	elif source == "local":
+		print("Loading data from local file...")
+		try:
+			data = pd.read_csv(dir, delimiter=",", index_col=0)
+		except FileNotFoundError:
+			raise SeriesNotFoundError(
+				"Local time series not found.")
+	else:
+		raise SeriesNotFoundError(
+			"Data source speficied not is not allowed.")
 
-	data = pd.read_csv(url, delimiter=",", index_col=0)
+	# Create data series collection.
 	ts = pd.Series(np.ravel(data.values), data.index, dtype=str)
-	ts[ts == "."] = np.nan
+	if any(ts == "."):
+		print("Missing data found, interpolate the missing data.")
+	ts[ts == "."] = np.nan  # Replace missing data with Nan.
 	ts = ts.astype(np.float32)
-	ts = ts.interpolate()
+	ts = ts.interpolate()  # Interpolate missing data.
 
 	TS = np.array(ts)
 	TS = TS.reshape(-1, 1)
@@ -79,39 +125,44 @@ def fetch_fred_single(target: str):
 	return ts, TS
 
 
-def fetch_local_single(dir: str):
-	print("Fetching data form local database...")
-	try:
-		data = pd.read_csv(dir, delimiter=",", index_col=0)
-	except FileNotFoundError:
-		raise SeriesNotFoundError("Local time series cannot be found.")
-
-	ts = pd.Series(np.ravel(data.values), data.index, dtype=str)
-	ts[ts == "."] = np.nan
-	ts = ts.astype(np.float32)
-	ts = ts.interpolate()
-
-	TS = np.array(ts)
-	TS = TS.reshape(-1, 1)
-	print("Done.")
-	return ts, TS
-
-# def gen_loss_tensor(
-# 	y_hat: tf.Tensor,
-# 	y: tf.Tensor,
-# 	metric: str="mse"
-# 	) -> (tf.Tensor, str):
-# 	loss_pack = {
-# 		"sse": tf.reduce_sum(tf.square(outputs - y), name="loss_sse"),
-# 		"mse": tf.reduce_mean(tf.square(outputs - y), name="loss_mse"),
-# 		"rmse": tf.sqrt(tf.reduce_mean(tf.square(outputs - y)), name="loss_rmse")
-# 	}
-# 	return loss_pack[metric], metric
-
-def test_data(series, forecast, num_periods, TS):
+def test_data(
+	series,
+	forecast,
+	num_periods,TS
+	) -> (np.ndarray, np.ndarray):
 	print("Generating testing data...")
 	test_x_setup = TS[-(num_periods + forecast):]
 	testX = test_x_setup[:num_periods].reshape(-1, num_periods, 1)
 	testY = TS[-(num_periods):].reshape(-1,num_periods,1)
 	print("Done.")
 	return testX, testY
+
+
+def visualize(
+	y_data: np.ndarray,  # Ground True value.
+	y_pred_train: np.ndarray,  # Predicted value on training set.
+	y_pred_test: np.ndarray,  # Predicted value on testing set.
+	on_server: bool=False  # If on AWS server.
+	) -> None:
+	
+	# Visualize test set.
+	pred = [None] * len(np.ravel(y_data))
+	pred[-len(np.ravel(y_pred)):] = np.ravel(y_pred)
+
+	plt.plot(pd.Series(np.ravel(y_data)), alpha=0.6, linewidth=0.5)
+	plt.plot(pd.Series(pred), alpha=0.8, linewidth=0.5)
+
+	if not para.on_server:
+		plt.show()
+
+	now_str = datetime.strftime(datetime.now(), "%Y_%m_%d_%s")
+	plt.savefig(f"./figure/result{now_str}_test.svg", format="svg")
+	plt.close()
+
+	full = [None] * len(np.ravel(y_data))
+	full[0:len(y_pred_train)] = y_pred_train
+	plt.plot(pd.Series(np.ravel(y_data)), alpha=0.6, linewidth=0.5)
+	plt.plot(pd.Series(np.ravel(full)), alpha=0.6, linewidth=0.5)
+	plt.savefig(f"./figure/result{now_str}_all.svg", format="svg")
+
+def visualize_error(loss_record: np.ndarray)
--- Time_series_python/alpha/test_lstm_stack.py
+++ Time_series_python/alpha/test_lstm_stack.py
@@ -1,9 +1,9 @@
-# https://medium.com/@anthony_sarkis/
-# tensorboard-quick-start-in-5-minutes-e3ec69f673af
+# Alpha Version
+# Tianyu Du
 
-# General Settings.
+# Loading Packages
 from meta import *
-para = parameter_control()
+para = ParameterControl()
 
 print("Loading Packages...")
 import tensorflow as tf
@@ -23,137 +23,125 @@ from sklearn import preprocessing
 
 print("Done.")
 
-ts, TS = fetch_local_single("./data/CPIAUCSL.csv")
+def main():
 
-scaler = preprocessing.StandardScaler().fit(TS)
-print("Scaler built.")
+	# Prepare data.
+	ts, TS = load_data("./data/CPIAUCSL.csv", "local")
 
-TS = scaler.transform(TS)
+	scaler = preprocessing.StandardScaler().fit(TS)
+	print("Scaler built.")
 
-num_periods = para.num_periods
-f_horizon = para.f_horizon  # Forecasting period.
+	TS = scaler.transform(TS)
 
-x_data = TS[:(len(TS) - (len(TS) % num_periods))]
-x_batches = x_data.reshape(-1, num_periods, 1)
+	num_periods = para.num_periods
+	f_horizon = para.f_horizon  # Forecasting period.
 
-y_data = TS[1: (len(TS) - (len(TS) % num_periods)) + 1]
-y_batches = y_data.reshape(-1, num_periods, 1)
+	x_data = TS[:(len(TS) - (len(TS) % num_periods))]
+	x_batches = x_data.reshape(-1, num_periods, 1)
 
-X_test, Y_test = test_data(TS, f_horizon, num_periods, TS)
+	y_data = TS[1: (len(TS) - (len(TS) % num_periods)) + 1]
+	y_batches = y_data.reshape(-1, num_periods, 1)
 
-tf.reset_default_graph()
-print("Default graph reset.")
+	X_test, Y_test = test_data(TS, f_horizon, num_periods, TS)
 
-inputs = 1
-hidden = [32, 64]
-output = 1
-
-# Input feed node.
-X = tf.placeholder(
-	tf.float32,
-	[None, num_periods, inputs],
-	name="input_label_feed_X")
-
-# Output node.
-y = tf.placeholder(tf.float32,
-	[None, num_periods, output],
-	name="output_label_feed_y")
-
-multi_layers = [
-	tf.nn.rnn_cell.BasicRNNCell(num_units=hidden[0]),
-	tf.nn.rnn_cell.LSTMCell(num_units=hidden[1], cell_clip=100)
-	]
-
-multi_cells = tf.nn.rnn_cell.MultiRNNCell(multi_layers)
-
-rnn_output, states = tf.nn.dynamic_rnn(
-	multi_cells,
-	inputs=X,
-	dtype=tf.float32)
+	tf.reset_default_graph()
+	print("Default graph reset.")
 
 
-stacked_rnn_output = tf.reshape(rnn_output, [-1, hidden[-1]], name="stacked_rnn_output")
-stacked_outputs = tf.layers.dense(stacked_rnn_output, output, name="stacked_outputs")
+	# Input feed node.
+	X = tf.placeholder(
+		tf.float32,
+		[None, num_periods, para.nn.inputs],
+		name="input_label_feed_X")
 
-outputs = tf.reshape(stacked_outputs, [-1, num_periods, output])
+	# Output node.
+	y = tf.placeholder(
+		tf.float32,
+		[None, num_periods, para.nn.output],
+		name="output_label_feed_y")
 
+	multi_layers = [
+		tf.nn.rnn_cell.BasicRNNCell(num_units=para.nn.hidden[0]),
+		tf.nn.rnn_cell.LSTMCell(num_units=para.nn.hidden[1], cell_clip=100)
+		]
 
-def gen_loss_tensor(
-	y_hat: tf.Tensor,
-	y: tf.Tensor,
-	metric: str="mse"
-	) -> (tf.Tensor, str):
-	loss_pack = {
-		"sse": tf.reduce_sum(tf.square(outputs - y), name="loss_sse"),
-		"mse": tf.reduce_mean(tf.square(outputs - y), name="loss_mse"),
-		"rmse": tf.sqrt(tf.reduce_mean(tf.square(outputs - y)), name="loss_rmse")
-	}
-	return loss_pack[metric], metric
+	multi_cells = tf.nn.rnn_cell.MultiRNNCell(multi_layers)
 
-
-loss, loss_metric = gen_loss_tensor(outputs, y, metric="mse")
-l2 = float(0.005) * sum(
-    tf.nn.l2_loss(tf_var)
-        for tf_var in tf.trainable_variables()
-        if not ("noreg" in tf_var.name or "Bias" in tf_var.name)
-)
-loss += l2
-loss_metric += " + reg"
-
-optimizer = tf.train.AdamOptimizer(learning_rate=para.learning_rate)
-training_op = optimizer.minimize(loss)
-
-init = tf.global_variables_initializer()
-
-with tf.Session() as sess:
-	writer = tf.summary.FileWriter("output", sess.graph)
-	# tf.summary.histogram("loss", loss)
-	# tf.summary.histogram("outputs", outputs)
-	init.run()
-	print("Tensors initialized.")
-	print("Training...")
-	begin_time = datetime.now()
-
-	loss_record = [1]
-	for ep in range(para.epochs):
-		sess.run(training_op, feed_dict={X: x_batches, y: y_batches})
-		if ep % 100 == 0:
-			quantified_loss = loss.eval(feed_dict={X: x_batches, y: y_batches})
-			loss_record.append(quantified_loss)
-			print(ep, f"\t{loss_metric}:", quantified_loss)
-			print(f"\tLoss Improvement: {-1 * ((loss_record[-1] - loss_record[-2]) / loss_record[-2] * 100)} %.")
-	y_pred = sess.run(outputs, feed_dict={X: X_test})
-	y_pred_train = sess.run(outputs, feed_dict={X: x_batches})
-	print(y_pred)
-	writer.close()
-
-	print("Finished, time taken {}.".format(datetime.now() - begin_time))
-
-y_pred = scaler.inverse_transform(y_pred)
-y_data = scaler.inverse_transform(y_data)
-y_pred_train = scaler.inverse_transform(y_pred_train)
-
-y_pred_train = y_pred_train.reshape(-1,1)
-
-pred = [None] * len(np.ravel(y_data))
-pred[-len(np.ravel(y_pred)):] = np.ravel(y_pred)
-
-plt.plot(pd.Series(np.ravel(y_data)), alpha=0.6, linewidth=0.5)
-plt.plot(pd.Series(pred), alpha=0.8, linewidth=0.5)
-
-if not para.on_server:
-	plt.show()
-
-now_str = datetime.strftime(datetime.now(), "%Y_%m_%d_%s")
-plt.savefig(f"./figure/result{now_str}_test.svg", format="svg")
-
-full = [None] * len(np.ravel(y_data))
-full[0:len(y_pred_train)] = y_pred_train
-plt.plot(pd.Series(np.ravel(y_data)), alpha=0.6, linewidth=0.5)
-plt.plot(pd.Series(np.ravel(full)), alpha=0.6, linewidth=0.5)
-plt.savefig(f"./figure/result{now_str}_all.svg", format="svg")
+	rnn_output, states = tf.nn.dynamic_rnn(
+		multi_cells,
+		inputs=X,
+		dtype=tf.float32)
 
 
+	stacked_rnn_output = tf.reshape(
+		rnn_output,
+		[-1, para.nn.hidden[-1]],
+		name="stacked_rnn_output"
+		)
 
+	stacked_outputs = tf.layers.dense(
+		stacked_rnn_output,
+		para.nn.output,
+		name="stacked_outputs"
+		)
+
+	outputs = tf.reshape(
+		stacked_outputs,
+		[-1, num_periods, para.nn.output]
+		)
+
+
+	loss, loss_metric = gen_loss_tensor(outputs, y, metric="mse")
+
+	loss = add_regularization(loss)
+
+	loss_metric += " + reg"
+
+	optimizer = tf.train.AdamOptimizer(learning_rate=para.learning_rate)
+	training_op = optimizer.minimize(loss)
+
+	init = tf.global_variables_initializer()
+
+	with tf.Session() as sess:
+		print("Starting session...")
+		print("Starting writer...")
+		writer = tf.summary.FileWriter("output", sess.graph)
+		tf.summary.histogram("loss", loss)
+		tf.summary.histogram("outputs", outputs)
+		print("Initializing tensors...")
+		init.run()
+		print("Training...")
+		begin_time = datetime.now()
+
+		loss_record = [1]
+		for ep in range(para.epochs):
+			sess.run(training_op, feed_dict={X: x_batches, y: y_batches})
+			if ep % 100 == 0:
+				quantified_loss = loss.eval(feed_dict={X: x_batches, y: y_batches})
+				loss_record.append(quantified_loss)
+				print(ep,
+					f"\t{loss_metric}:", quantified_loss)
+				print(
+					f"\tLoss Improvement: {-1 * ((loss_record[-1] - loss_record[-2]) / loss_record[-2] * 100)} %.")
+		y_pred_train = sess.run(outputs, feed_dict={X: x_batches})
+		y_pred_test = sess.run(outputs, feed_dict={X: X_test})
+		writer.close()
+
+		print("Finished, time taken {}.".format(datetime.now() - begin_time))
+
+	# Transform back
+	y_data = scaler.inverse_transform(y_data)
+	y_pred_train = scaler.inverse_transform(y_pred_train)
+	y_pred_train = y_pred_train.reshape(-1,1)  # Expand the stacked inputs.
+	y_pred_test = scaler.inverse_transform(y_pred_tests)
+
+	visualize(
+		y_data,
+		y_pred_train,
+		y_pred_test,
+		on_server=para.on_server)
+
+if __name__ == "__main__":
+	main()
 
 
