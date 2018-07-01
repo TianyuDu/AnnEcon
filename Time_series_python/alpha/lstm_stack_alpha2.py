d671443fe1d5a4deac9725701671993f6dea0ebb
MasterDoo <masterdu9951@icloud.com>
2018-07-01 03:19:56 -0400

    Upload from Mac.local

---
+++ Time_series_python/alpha/lstm_stack_alpha2.py
@@ -0,0 +1,97 @@
+"""
+Alpha version 2
+"""
+# Loading Packages
+from meta import *
+from classes import *
+para = ParameterControl()
+print("Loading Packages...")
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+from datetime import datetime
+from sklearn import preprocessing
+import matplotlib
+if para.on_server:
+    matplotlib.use(
+        "agg",
+        warn=False,
+        force=True
+        )  # If on a server, change matplotlib settings.
+import matplotlib.pyplot as plt
+
+print("Done.")
+
+
+def main(parameters: "ParameterControl"):
+    """
+    Main operation
+    """
+    print("@main: version alpha 2: single time series prediction, stacked.")
+    # Prepare data.
+    print("@main: preparing data...")
+    print("\t@main: CPIAUCSL data will be loaded, as benchmark test data.")
+    ts, ts_array = load_data("./data/CPIAUCSL.csv", "local")
+
+    # Reset tensorflow
+    print("@main: resetting default model ")
+    tf.reset_default_graph()
+    # Create model.
+    print("@main: building model...")
+    model = StackedRnnModel(ts_array, parameters=parameters)
+
+    with tf.Session() as sess:
+        print("@main: Starting session...")
+        print("@main: Starting writer...")
+        writer = tf.summary.FileWriter("output", sess.graph)
+        tf.summary.histogram("loss", model.loss)
+        tf.summary.histogram("outputs", model.outputs)
+        print("@main: Initializing tensors...")
+        model.init.run()
+        print("@main: Training...")
+        begin_time = datetime.now()
+
+        loss_record = [1]  # Record loss history.
+
+        for ep in range(para.epochs):
+            sess.run(
+                model.training_operation,
+                feed_dict={
+                    model.X: model.x_batches,
+                    model.y: model.y_batches})
+
+            if ep % 100 == 0:
+                quantified_loss = model.loss.eval(
+                    feed_dict={model.X: model.x_batches,
+                               model.y: model.y_batches})
+                loss_record.append(quantified_loss)
+                print(ep,
+                      f"\t{model.loss_metric}: {quantified_loss}")
+                print(
+                    f"\tLoss Improvement:\
+                     {-1 * ((loss_record[-1] - loss_record[-2]) / loss_record[-2] * 100)} %.")
+
+        # Create training set prediction,
+        y_pred_train = sess.run(
+            model.outputs, feed_dict={model.X: model.x_batches})
+        y_pred_test = sess.run(
+            model.outputs, feed_dict={model.X: model.X_test})
+        writer.close()
+
+        print(f"Training finished, time taken {(datetime.now() - begin_time)}")
+
+    # Transform back
+    y_data = model.scaler.inverse_transform(model.y_data)
+    y_pred_train = model.scaler.inverse_transform(y_pred_train)
+    y_pred_train = y_pred_train.reshape(-1, 1)  # Expand the stacked inputs.
+    y_pred_test = model.scaler.inverse_transform(y_pred_test)
+
+    visualize(
+        y_data,
+        y_pred_train,
+        y_pred_test,
+        on_server=parameters.on_server)
+
+
+if __name__ == "__main__":
+    main(para)
--- Time_series_python/alpha/meta.py
+++ Time_series_python/alpha/meta.py
@@ -1,169 +1,145 @@
+"""
+Meta methods and classes for model to use.
+"""
 import tensorflow as tf
 import numpy as np
 import pandas as pd
 from datetime import datetime
-from sklearn import preprocessing
 import matplotlib.pyplot as plt
 
-
-class ParameterControl():
-	"""
-	This logger is used to collect all user-specified
-	parameters in training.
-	"""
-	on_server: int  # If on AWS server.
-	num_periods: int
-	f_horizon: int
-	learning_rate: float
-	epochs: int
-
-	def __init__(self):
-		self.on_server = int(
-			input("On Server? [0/1]: "))
-		if int(input("Use default parameters [0/1]: ")):
-			print("Setting parameters to default...")
-			self.num_periods = 24
-			self.f_horizon = 1
-			self.learning_rate = 0.001
-			self.epochs = 100
-		else:
-			self.get_parameters()
-		self.load_customized_parameters()
-
-	def get_parameters(self):
-		print("Please input training parameters")
-		self.num_periods = int(
-			input("Number of periods looking back in traning: ")
-			)
-		self.f_horizon = int(
-			input("Numer of periods to forecast: ")
-			)
-		self.learning_rate = float(
-			input("Learning rate: ")
-			)
-		self.epochs = int(
-			input("Training epochs: ")
-			)
-
-	def load_customized_parameters(self):
-		self.nn_inputs = 1
-		self.nn_hidden = [64, 128]
-		self.nn_output = 1
-		self.nn_reg_para = 0.005
-
-class SeriesNotFoundError(Exception):
-	"""
-	The Error Risen to indicate
-	"""
-	pass
+from classes import *
+from predefined import *
 
 
-def add_regularization(loss: tf.Tensor, para: "ParameterControl") -> (tf.Tensor):
-	l2 = float(para.nn_reg_para) * sum(
-	    tf.nn.l2_loss(tf_var)
-	        for tf_var in tf.trainable_variables()
-	        if not ("noreg" in tf_var.name or "Bias" in tf_var.name)
-	)
-	return loss + l2
+def add_regularization(loss: tf.Tensor, para: "ParameterControl") -> tf.Tensor:
+    """
+    Add regularization term to he loss tensor.
+    """
+    l2 = float(para.nn["reg_para"]) * sum(
+        tf.nn.l2_loss(tf_var)
+            for tf_var in tf.trainable_variables()
+            if not ("noreg" in tf_var.name or "Bias" in tf_var.name)
+    )
+    return tf.add(loss, l2)
+
 
 def gen_loss_tensor(
-	y_hat: tf.Tensor,
-	y: tf.Tensor,
-	metric: str="mse",
-	) -> (tf.Tensor, str):
-	loss_pack = {
-		"sse": tf.reduce_sum(tf.square(y_hat - y), name="loss_sse"),
-		"mse": tf.reduce_mean(tf.square(y_hat - y), name="loss_mse"),
-		"rmse": tf.sqrt(tf.reduce_mean(tf.square(y_hat - y)), name="loss_rmse")
-	}
-	return loss_pack[metric], metric
+    y_hat: tf.Tensor,
+    y: tf.Tensor,
+    metric: str="mse",
+    ) -> (tf.Tensor, str):
+    """
+    Generate specific type of loss tensor based on predicted and ground truth tensor.
+
+    metric:
+        sse := sum square error.
+        mse := mean squared error.
+        rmse := root mean squared error.
+        mae := mean absolute error.
+    """
+    loss_pack = {
+        "sse": tf.reduce_sum(tf.square(y_hat - y), name="loss_sse"),
+        "mse": tf.reduce_mean(tf.square(y_hat - y), name="loss_mse"),
+        "rmse": tf.sqrt(tf.reduce_mean(tf.square(y_hat - y)), name="loss_rmse"),
+        "mae": tf.reduce_sum(tf.abs(y_hat - y), name="loss_mae")
+    }
+    return loss_pack[metric], metric
 
 
 def load_data(
-	target: str,
-	source: str
-	) -> (pd.Series, np.ndarray):
-	"""
-		source:
-			"fred": download data from fred database.
-			"local": load data from local csv file.
-	"""
-	fred_url_package = {
-		"SP500": "https://fred.stlouisfed.org/series/SP500/downloaddata/SP500.csv",
-		"CPIAUCSL": "https://fred.stlouisfed.org/series/CPIAUCSL/downloaddata/CPIAUCSL.csv"
-	}
-	if source == "fred":
-		print("Fetching data from Fred database...")
-		try:
-			url = fred_url_package[target]
-			data = pd.read_csv(url, delimiter=",", index_col=0)
-		except KeyError:
-			raise SeriesNotFoundError(
-				"Time series requested not found in data base.")
-	elif source == "local":
-		print("Loading data from local file...")
-		try:
-			data = pd.read_csv(target, delimiter=",", index_col=0)
-		except FileNotFoundError:
-			raise SeriesNotFoundError(
-				"Local time series not found.")
-	else:
-		raise SeriesNotFoundError(
-			"Data source speficied not is not allowed.")
+    target: str,
+    source: str
+    ) -> (pd.Series, np.ndarray):
+    """
+    source:
+        fred := download data from fred database.
+        local := load data from local csv file.
+    """
+    fred_url_package = {
+        "SP500": "https://fred.stlouisfed.org/series/SP500/downloaddata/SP500.csv",
+        "CPIAUCSL": "https://fred.stlouisfed.org/series/CPIAUCSL/downloaddata/CPIAUCSL.csv"
+    }
+    if source == "fred":
+        print("Fetching data from Fred database...")
+        try:
+            url = fred_url_package[target]
+            data = pd.read_csv(url, delimiter=",", index_col=0)
+        except KeyError:
+            raise SeriesNotFoundError(
+                "Time series requested not found in data base.")
+    elif source == "local":
+        print("Loading data from local file...")
+        try:
+            data = pd.read_csv(target, delimiter=",", index_col=0)
+        except FileNotFoundError:
+            raise SeriesNotFoundError(
+                "Local time series not found.")
+    else:
+        raise SeriesNotFoundError("Data source speficied not is not allowed.")
 
-	# Create data series collection.
-	ts = pd.Series(np.ravel(data.values), data.index, dtype=str)
-	if any(ts == "."):
-		print("Missing data found, interpolate the missing data.")
-	ts[ts == "."] = np.nan  # Replace missing data with Nan.
-	ts = ts.astype(np.float32)
-	ts = ts.interpolate()  # Interpolate missing data.
+    # Create data series collection.
+    ts = pd.Series(np.ravel(data.values), data.index, dtype=str)
+    if any(ts == "."):
+        print("Missing data found, interpolate the missing data.")
+    ts[ts == "."] = np.nan  # Replace missing data with Nan.
+    ts = ts.astype(np.float32)
+    ts = ts.interpolate()  # Interpolate missing data.
 
-	TS = np.array(ts)
-	TS = TS.reshape(-1, 1)
-	print("Done.")
-	return ts, TS
+    series = np.array(ts)
+    series = series.reshape(-1, 1)
+    print("Done.")
+    return ts, series
 
 
 def test_data(
-	series,
-	forecast,
-	num_periods,TS
-	) -> (np.ndarray, np.ndarray):
-	print("Generating testing data...")
-	test_x_setup = TS[-(num_periods + forecast):]
-	testX = test_x_setup[:num_periods].reshape(-1, num_periods, 1)
-	testY = TS[-(num_periods):].reshape(-1,num_periods,1)
-	print("Done.")
-	return testX, testY
+        series: np.ndarray,
+        forecast: int,
+        num_periods: int
+            ) -> (np.ndarray, np.ndarray):
+    """
+    Generating test data.
+    """
+    print("Generating testing data...")
+    test_x_setup = series[-(num_periods + forecast):]
+    test_x = test_x_setup[:num_periods].reshape(-1, num_periods, 1)
+    test_y = series[-num_periods:].reshape(-1,num_periods,1)
+    print("Done.")
+    return test_x, test_y
 
 
 def visualize(
-	y_data: np.ndarray,  # Ground True value.
-	y_pred_train: np.ndarray,  # Predicted value on training set.
-	y_pred_test: np.ndarray,  # Predicted value on testing set.
-	on_server: bool=False  # If on AWS server.
-	) -> None:
+        y_data: np.ndarray,  # Ground True value.
+        y_pred_train: np.ndarray,  # Predicted value on training set.
+        y_pred_test: np.ndarray,  # Predicted value on testing set.
+        on_server: bool=False  # If on AWS server.
+            ) -> None:
+    """
+    Visualize result.
+    """
 
-	# Visualize test set.
-	pred = [None] * len(np.ravel(y_data))
-	pred[-len(np.ravel(y_pred_test)):] = np.ravel(y_pred_test)
+    # Visualize test set.
+    pred = [None] * len(np.ravel(y_data))
+    pred[-len(np.ravel(y_pred_test)):] = np.ravel(y_pred_test)
 
-	plt.plot(pd.Series(np.ravel(y_data)), alpha=0.6, linewidth=0.5)
-	plt.plot(pd.Series(pred), alpha=0.8, linewidth=0.5)
+    plt.plot(pd.Series(np.ravel(y_data)), alpha=0.6, linewidth=0.5)
+    plt.plot(pd.Series(pred), alpha=0.8, linewidth=0.5)
 
-	if not on_server:
-		plt.show()
+    if not on_server:
+        plt.show()
 
-	now_str = datetime.strftime(datetime.now(), "%Y_%m_%d_%s")
-	plt.savefig(f"./figure/result{now_str}_test.svg", format="svg")
-	plt.close()
+    now_str = datetime.strftime(datetime.now(), "%Y_%m_%d_%s")
+    plt.savefig(f"./figure/result{now_str}_test.svg", format="svg")
+    plt.close()
 
-	full = [None] * len(np.ravel(y_data))
-	full[0:len(y_pred_train)] = y_pred_train
-	plt.plot(pd.Series(np.ravel(y_data)), alpha=0.6, linewidth=0.5)
-	plt.plot(pd.Series(np.ravel(full)), alpha=0.6, linewidth=0.5)
-	plt.savefig(f"./figure/result{now_str}_all.svg", format="svg")
+    full = [None] * len(np.ravel(y_data))
+    full[0:len(y_pred_train)] = y_pred_train
+    plt.plot(pd.Series(np.ravel(y_data)), alpha=0.6, linewidth=0.5)
+    plt.plot(pd.Series(np.ravel(full)), alpha=0.6, linewidth=0.5)
+    plt.savefig(f"./figure/result{now_str}_all.svg", format="svg")
 
-def visualize_error(loss_record: np.ndarray):
-	pass
+
+def visualize_error(loss_record: np.ndarray) -> None:
+    """
+    Visualize error and training progress.
+    """
+    raise NotImplementedError
