e6a6c0fbb4d1bd1f9140b6f551df8223f7589f27
MasterDoo <masterdu9951@icloud.com>
2018-07-02 17:04:56 -0400

    Uploading from Mac.local

--- Time_series_python/playground/pandas_panel.py
+++ Time_series_python/playground/pandas_panel.py
@@ -52,26 +52,53 @@ ts_rgdp = pd.Series(np.ravel(rgdp.values), index=pd.to_datetime(rgdp.index))
 ts_unrate = pd.Series(np.ravel(unrate.values), index=pd.to_datetime(unrate.index))
 
 
-def load_time_table(var_list: List[str],
-                    source: Dict[str, str],
-                    freq: str="M"):
+class TimeTable:
     """
     [Experimental] Create time serie table containing indicators.
     """
-    print("@load_time_table: Loading time series...\n\tSeries to be loaded: ")
-    for var in var_list:
-        print("\t\t"+var)
+    def __init__(self, var_list: List[str], source: Dict[str, str], freq: str="M"):
+        print("@TimeTable: Initializing time series...\n\tSeries to be loaded: ")
+        for var in var_list:
+            print("\t\t"+var)
 
-    series_collection = dict()
-    for var in var_list:
-        assert var in source.keys(), "Variable/indicator requested are not provided "
-        try:
-            data = pd.read_csv(source[var], delimiter=",", index_col=0)
-            series = pd.Series(np.ravel(data.values), index=pd.to_datetime(data.index))
-            series_collection[var] = series
-        except FileNotFoundError:
-            warn(f"@load_time_table: Time series {var} cannot be loaded, action: skipped.")
-    print(f"@load_time_table: {len(series_collection)} series loaded successfully.")
+        self.series_collection = dict()
+        for var in var_list:
+            assert var in source.keys(), "Variable/indicator requested are not provided "
+            try:
+                data = pd.read_csv(source[var], delimiter=",", index_col=0)
+                series = pd.Series(np.ravel(data.values), index=pd.to_datetime(data.index))
+                self.series_collection[var] = series
+            except FileNotFoundError:
+                warn(f"\t@TimeTable: Time series {var} cannot be loaded, action: skipped.")
+        self.length = len(self.series_collection)
+        print(f"\t@TimeTable: {self.length} series loaded successfully.")
+
+
+        print("\t@Timetable: merging timetable.")
+        merged = self.series_collection.values()[0]
+        for i in range(1, self.length):
+            merged = np.merge(merged,
+                              self.series_collection.values()[i],
+                              how="left",
+                              left_index=True,
+                              right_on="DATE")
+
+        self.missing_idx = np.any(merged.isnull.values, 1)
+        print(f"\t@TimeTable: total time step sampled: {merged.values.shape}")
+        print(f"\t@TimeTable: number of time step containing missing data: {len(self.missing_idx)}")
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
