2c3214fe54c2b8775b8d0ca4ae4dadc66c1b0f0a
MasterDoo <masterdu9951@icloud.com>
2018-08-25 09:21:15 +0800

    adding existing file

--- k models/hello_k.py
+++ k models/hello_k.py
@@ -5,3 +5,9 @@ import numpy as np
 import keras
 from keras.models import Sequential
 from keras.layers import Dense, Activation
+
+model = Sequential()
+model.add(
+    Dense(32, input_shape=(784, ))
+    )
+
