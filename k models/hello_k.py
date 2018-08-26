84fe30857c5da33fd43c652b23f337cce85789d1
MasterDoo <masterdu9951@icloud.com>
2018-08-26 10:50:03 +0800

    adding existing file

--- k models/hello_k.py
+++ k models/hello_k.py
@@ -1,13 +1,42 @@
 """
-Experimental sequential model using Keras
+Experimental sequential models using Keras.
 """
-import numpy as np
-import keras
+
 from keras.models import Sequential
-from keras.layers import Dense, Activation
+from keras.layers import LSTM, Dense
+import numpy as np
+
+data_dim = 16
+timesteps = 8
+num_classes = 10
 
 model = Sequential()
 model.add(
-    Dense(32, input_shape=(784, ))
-    )
+    LSTM(32, return_sequences=True,
+    input_shape=(timesteps, data_dim))
+)
 
+model.add(
+    LSTM(32, return_sequences=True)
+)
+
+model.add(LSTM(32))
+model.add(Dense(10, activation="softmax"))
+model.compile(
+    loss="categorical_crossentropy",
+    optimizer="rmsprop",
+    metrics=["accuracy"])
+
+x_train = np.random.random((1000, timesteps, data_dim))
+y_train = np.random.random((1000, num_classes))
+
+x_val = np.random.random((100, timesteps, data_dim))
+y_val = np.random.random((100, num_classes))
+
+model.fit(
+    x_train,
+    y_train,
+    batch_size=64,
+    epochs=5,
+    validation_data=(x_val, y_val)
+)
