9db6cbe54877f0aa35089edc2d3e5d3c552cf188
MasterDoo <masterdu9951@icloud.com>
2018-09-06 00:39:30 -0400

    add multi-input LSTM model in tutorial

--- k models/multi_exchange/tutorial.py
+++ k models/multi_exchange/tutorial.py
@@ -7,6 +7,7 @@ from datetime import datetime
 import keras
 import pandas as pd
 from matplotlib import pyplot as plt
+import sklearn
 
 sys.path.append("/Users/tianyudu/Documents/Github/AnnEcon/k models/exchange")
 import config
@@ -37,7 +38,7 @@ dataset.to_csv("./data/pollution.csv")
 # Data cleaned, create new csv file to store the new data.
 
 # load dataset
-dataset = pd.read_csv("./data/pollution.csv", header=0, index_col=0)
+dataset = pd.read_csv("./data/pollution.csv", header=0, index_col=0, engine="c")
 values = dataset.values
 
 # specify columns to plot
@@ -54,3 +55,42 @@ plt.show()
 
 
 # LSTM Data Preparation
+def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
+    n_vars = 1 if type(data) is list else data.shape[1]
+    df = pd.DataFrame(data)
+    cols, names = list(), list()
+    # input seq. (t-n, ..., t-1)
+    # sup learning over n lagged vars.
+    for i in range(n_in, 0, -1):
+        cols.append(df.shift(i))
+        names += [(f"var{j+1, i}") for j in range(n_vars)]
+    
+    for i in range(0, n_out):
+        cols.append(df.shift(-i))
+        if i == 0:
+            names += [("var%d(t)" % (j+1)) for j in range(n_vars)]
+        else:
+            names += [("var%d(t+%d)" % (j+1, i)) for j in range(n_vars)]
+    
+    # put it all together.
+    agg = pd.concat(cols, axis=1)
+    agg.columns = names
+    # drop rows with NaN values.
+    if dropnan:
+        agg.dropna(inplace=True)
+    return agg
+
+
+# Load dataset
+dataset = pd.read_csv("./data/pollution.csv", header=0, index_col=0)
+values = dataset.values
+# integer encode direction
+encoder = sklearn.preprocessing.LabelEncoder()
+# encode wind_direction
+values[:, 4] = encoder.fit_transform(values[:, 4])
+values = values.astype(np.float32)
+# normalize
+scaler = sklearn.preprocessing.MinMaxScaler(feature_range=(0, 1))
+scaled = scaler.fit_transform(values)
+
+reframed =series_to_supervised(scaled, 1, 1) 
